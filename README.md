63ddb611c1570517e7290e78133fd97362043e9e
Lonense <41316884+Lonense@users.noreply.github.com>
2021-11-08 12:49:27 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,70 @@
+# holiday-cn
+
+[![Build Status](https://github.com/Lonese/holiday-cn/workflows/CI/badge.svg)](https://github.com/Lonese/holiday-cn/actions)
+[![Release](https://img.shields.io/github/release/Lonese/holiday-cn.svg)](https://github.com/Lonese/holiday-cn/releases/latest)
+[![CalVer](https://img.shields.io/badge/calver-YYYY.0M.0D-22bfda.svg)](http://calver.org)
+[![Maintainability](https://api.codeclimate.com/v1/badges/c8e9d9c51bd2d858c577/maintainability)](https://codeclimate.com/github/Lonese/holiday-cn/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/c8e9d9c51bd2d858c577/test_coverage)](https://codeclimate.com/github/Lonese/holiday-cn/test_coverage)
+![Maintenance](https://img.shields.io/maintenance/yes/2021.svg)
+
+中国法定节假日数据 自动每日抓取国务院公告
+
+- [x] 提供 JSON 格式节假日数据
+- [x] CI 自动更新
+- [x] 数据变化时自动发布新版本 ( `Watch` - `Release only` 以获取邮件提醒! )
+- [x] [发布页面]提供 JSON 打包下载
+
+数据格式:
+
+[JSON Schema](./schema.json)
+
+```TypeScript
+interface Holidays {
+  /** 完整年份, 整数。*/
+  year: number;
+  /** 所用国务院文件网址列表 */
+  papers: string[];
+  days: {
+    /** 节日名称 */
+    name: string;
+    /** 日期, ISO 8601 格式 */
+    date: string;
+    /** 是否为休息日 */
+    isOffDay: boolean;
+  }[]
+}
+```
+
+## 注意事项
+
+- 年份是按照国务院文件标题年份而不是日期年份，12 月份的日期可能会被下一年的文件影响，因此应检查两个文件。
+
+## 通过互联网使用
+
+数据地址格式:
+
+    https://raw.githubusercontent.com/Lonese/holiday-cn/master/{年份}.json
+
+访问 github 不方便时可使用国内镜像仓库
+
+    https://Lonese.coding.net/p/github/d/holiday-cn/git/raw/master/{年份}.json
+
+或使用 JSDelivr：
+ 
+    https://cdn.jsdelivr.net/gh/Lonese/holiday-cn@master/{年份}.json
+
+## ICalendar 订阅
+
+网址格式参见上一节
+
+`{年份}.ics` 为对应年份的节假日
+
+`holiday-cn.ics` 为 3 年前至次年的节假日
+
+感谢 @retanoj 的 ics 格式转换实现
+
+## 作为 git 子模块使用
+
+参见 [Git 工具 - 子模块](https://git-scm.com/book/zh/v2/Git-%E5%B7%A5%E5%85%B7-%E5%AD%90%E6%A8%A1%E5%9D%97)
+
+[发布页面]: https://github.com/Lonese/holiday-cn/releases
---
+++ generate_ics.py
@@ -0,0 +1,79 @@
+import datetime
+from typing import Any, Iterator, Sequence, Text, Tuple
+from icalendar import Event, Calendar, Timezone, TimezoneStandard
+import requests
+
+def _create_timezone():
+    tz = Timezone()
+    tz.add("TZID", "Asia/Shanghai")
+
+    tz_standard = TimezoneStandard()
+    tz_standard.add("DTSTART", datetime.datetime(1970, 1, 1))
+    tz_standard.add("TZOFFSETFROM", datetime.timedelta(hours=8))
+    tz_standard.add("TZOFFSETTO", datetime.timedelta(hours=8))
+
+    tz.add_component(tz_standard)
+    return tz
+
+
+def _create_event(event_name, start, end):
+    # 创建事件/日程
+    event = Event()
+    event.add("SUMMARY", event_name)
+
+    event.add("DTSTART", start)
+    event.add("DTEND", end)
+    # 创建时间
+    event.add("DTSTAMP", start)
+
+    # UID保证唯一
+    event["UID"] = f"{start}/{end}/Lonese/holiday-cn"
+
+    return event
+
+
+def _cast_date(v: Any) -> datetime.date:
+    if isinstance(v, datetime.date):
+        return v
+    if isinstance(v, str):
+        return datetime.date.fromisoformat(v)
+    raise NotImplementedError("can not convert to date: %s" % v)
+
+
+def _iter_date_ranges(days: Sequence[dict]) -> Iterator[Tuple[dict, dict]]:
+    if len(days) == 0:
+        return
+
+    if len(days) == 1:
+        yield days[0], days[0]
+        return
+
+    fr, to = days[0], days[0]
+    for cur in days[1:]:
+        if (_cast_date(cur["date"]) - _cast_date(to["date"])).days == 1 and cur[
+            "isOffDay"
+        ] == to["isOffDay"]:
+            to = cur
+        else:
+            yield fr, to
+            fr, to = cur, cur
+    yield fr, to
+
+
+def generate_ics(days: Sequence[dict], filename: Text) -> None:
+    """Generate ics from days."""
+    url = 'https://p10-calendars.icloud.com/holiday/CN_zh.ics'
+    cal = Calendar.from_ical(requests.get(url).text)
+    days = sorted(days, key=lambda x: x["date"])
+
+    for fr, to in _iter_date_ranges(days):
+        start = _cast_date(fr["date"])
+        end = _cast_date(to["date"]) + datetime.timedelta(days=1)
+
+        name = fr["name"] + "假期"
+        if not fr["isOffDay"]:
+            name = "上班(补" + name + ")"
+        cal.add_component(_create_event(name, start, end))
+
+    with open(filename, "wb") as f:
+        f.write(cal.to_ical())
